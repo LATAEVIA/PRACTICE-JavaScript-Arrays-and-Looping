2e344723b808c054217c7b6fb7a4a639fb8b48b7
LaTaevia <lataevia.berry@gmail.com>
2016-03-30 11:58:06 -0700

    add non-finctioning, incomplete icecream project

---
+++ iceCream/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  var iceCreams = ['strawberry', 'butter pecan', 'more strawberry'];
+
+    iceCreams.forEach(function(iceCream) {
+      $('favOne').text(iceCream [1]);
+      $('li').text(iceCream[1]);
+      $('favThree').text(iceCream);
+    });
+  });
